4653dec5542166af66ec35264faed3085895696a
Leo Driesch <leo.driesch@gmail.com>
2020-02-22 21:42:08 +0100

    :truck: Extract helper to delete firestore collections

---
+++ src/index/deleteFirebaseCollection.ts
@@ -0,0 +1,17 @@
+import * as admin from 'firebase-admin';
+
+const deleteFirebaseCollection = async (
+    batch: FirebaseFirestore.WriteBatch,
+    path: string,
+): Promise<void> => {
+    const documents = await admin
+        .firestore()
+        .collection(path)
+        .listDocuments();
+
+    documents.map(val => {
+        batch.delete(val);
+    });
+};
+
+export default deleteFirebaseCollection;
