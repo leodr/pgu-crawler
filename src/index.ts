0d1d8e53db26fc7e9fea6f0b5e366bc8fcad1fd6
Leo Driesch <leo.driesch@gmail.com>
2020-02-22 21:41:49 +0100

    :sparkles: Write data into Cloud Firestore

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,58 @@
-import cheerio from 'cheerio';
-import fetch from 'node-fetch';
+import * as admin from 'firebase-admin';
+import hash from 'object-hash';
+import firebaseCredentials from '../firebase-credentials.json';
+import deleteFirebaseCollection from './index/deleteFirebaseCollection';
+import getCalendar from './index/getCalendar';
+import getCanteen from './index/getCanteen';
+import getNews from './index/getNews';
 
-const NEWS_URL = 'http://www.pgu.de/fileadmin/Vertretungsplan/Neu/Forum_4.php';
-
-const fetchHTML = async (url: string): Promise<string> => {
-    const response = await fetch(url);
-    const html = await response.text();
-    return html;
+const serviceAccount = {
+    type: firebaseCredentials.type,
+    projectId: firebaseCredentials.project_id,
+    privateKeyId: firebaseCredentials.private_key_id,
+    privateKey: firebaseCredentials.private_key,
+    clientEmail: firebaseCredentials.client_email,
+    clientId: firebaseCredentials.client_id,
+    authUri: firebaseCredentials.auth_uri,
+    tokenUri: firebaseCredentials.token_uri,
+    authProviderX509CertUrl: firebaseCredentials.auth_provider_x509_cert_url,
+    clientC509CertUrl: firebaseCredentials.client_x509_cert_url,
 };
 
-const getArticleUrls = async (): Promise<string[]> => {
-    const html = await fetchHTML(NEWS_URL);
-    const $ = cheerio.load(html);
-    const newsElements = $('.news-list-item > a');
-
-    return newsElements
-        .map((i, el) => {
-            return el.attribs.href;
-        })
-        .get();
-};
-
-const main = async () => {
-    const articleUrls = await getArticleUrls();
-
-    const articleObjects = articleUrls.map(async url => {
-        const html = await fetchHTML(url);
-        const $ = cheerio.load(html);
-        const newsItem = $('.news-single-item');
-        const heading = newsItem.find('h1').text();
-        const date = newsItem
-            .find('.news-single-rightbox')
-            .text()
-            .trim();
-        const children = $('.news-single-item > hr').first.nextUntil('hr');
-
-        return { heading, date };
+const main = async (): Promise<void> => {
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: 'https://pgu-app.firebaseio.com',
     });
 
-    console.log(await Promise.all(articleObjects));
+    const news = await getNews();
+    const calendarEntries = await getCalendar();
+    const canteenUrl = await getCanteen();
+
+    const db = admin.firestore();
+    const batch = db.batch();
+
+    for (const newsArticle of news) {
+        const docRef = db.collection('news').doc(newsArticle.slug);
+
+        batch.set(docRef, newsArticle);
+    }
+
+    await deleteFirebaseCollection(batch, 'calendar');
+
+    for (const calendarEntry of calendarEntries) {
+        const key = hash(calendarEntry, { algorithm: 'md5' });
+
+        const calendarRef = db.collection('calendar').doc(key);
+
+        batch.set(calendarRef, calendarEntry);
+    }
+
+    const canteenUrlRef = db.collection('canteen').doc('canteenUrl');
+
+    batch.set(canteenUrlRef, { canteenUrl });
+
+    batch.commit();
 };
 
 main();
